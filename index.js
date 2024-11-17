d38ff5f517d50bd572913d37ee65500d8221d03d
farhanazizf-wartek <farhan.aziz@wartek.belajar.id>
2024-11-17 08:48:38 +0700

    fix: cors

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const { sequelize, initModels } = require("./models");
 const cors = require("cors");
 
 dotenv.config();
-app.use(cors());
 
 const app = express();
+app.use(cors());
+
 app.use(express.json());
 app.use("/api", routes);
 
